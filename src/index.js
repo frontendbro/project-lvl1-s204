1b5fe2fe60b5e782c6bb8ceb0b82290a344c99ff
Simon <simon.z@yandex.ru>
2017-11-02 00:35:24 +0300

    stage_3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
-console.log("$ src/bin/brain-games.js \nWelcome to the Brain Games!");
+console.log('$ src/bin/brain-games.js \nWelcome to the Brain Games!');
 
 const actual = readlineSync.question('May I have your name? ');
-console.log("Hello " + actual);
+
+console.log('Hello ' + actual);
